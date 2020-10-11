97c26d74d95fca8adc1060cca41c3965590e5974
rohrlaf <rohrlaf@mailbox.org>
2020-10-11 11:49:54 +0200

    Add first component: One

---
+++ src/pages/One/One.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Chart from "../../components/Chart/Chart";
+import { getPowerEfficiencyOne } from "../../api/power-efficiency";
+
+const One = () => {
+  const [data, setData] = useState({});
+
+  const getData = async () => {
+    const data = await getPowerEfficiencyOne();
+    setData(data?.data);
+  };
+
+  useEffect(() => {
+    getData();
+  });
+
+  return (
+    <>
+      <h1>Dataset: One</h1>
+      <Chart data={data} title="Dataset: One" />
+    </>
+  );
+};
+
+export default One;
